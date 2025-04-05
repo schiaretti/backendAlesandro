39ca6eef844d55ee743c694babf7a38f19291738
ronischiaretti <pnamaquina@gmail.com>
2025-04-05 16:18:06 -0300

    ajustei a rota de postes para salvar as outras fotos coloquei no enum

--- routes/public.js
+++ routes/public.js
@@ -179,7 +179,7 @@ router.post('/postes', handleUpload('fotos', 5), async (req, res) => {
         }
 
         // 3. Validação das fotos obrigatórias
-        const requiredPhotos = ['PANORAMICA', 'LUMINARIA', 'ARVORE', 'TELECON', '2TIPOLAMPADA'];
+        const requiredPhotos = ['PANORAMICA', 'LUMINARIA', 'ARVORE', 'TELECON', 'LAMPADA'];
         const photoTypes = Array.isArray(body.tipo_fotos) ? body.tipo_fotos :
             body.tipo_fotos ? [body.tipo_fotos] : [];
 
